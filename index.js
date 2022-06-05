c3476fc338a8cb815bc0a1ca516b949a0afb503e
Abdulozy <abdulozy007@gmail.com>
2022-06-05 23:06:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function countdown(i) {
+    //initialize the variable to be returned with the initial i
+    var ret = i;
+    //in each iteration, assigns i to i-1 then checks if i >= 0
+    while (--i >= 0) {
+        //concatenates a space and the current i value to the return string
+        ret += ' ' + i;
+    }
+    //returns the string
+    return ret;
+}
