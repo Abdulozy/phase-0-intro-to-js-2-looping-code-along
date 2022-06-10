38184e397425171997a2d53f8a03b3d3b4da9ed8
Abdulozy <abdulozy007@gmail.com>
2022-06-10 10:04:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Code your solutions in this file
-function countdown(i) {
-    //initialize the variable to be returned with the initial i
-    var ret = i;
-    //in each iteration, assigns i to i-1 then checks if i >= 0
-    while (--i >= 0) {
-        //concatenates a space and the current i value to the return string
-        ret += ' ' + i;
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
     }
-    //returns the string
-    return ret;
-}
+    return thankYouCards
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
